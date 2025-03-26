adcde6e6220a226b274bbc945c89c74f3f71319a
Felex Kuria <engineerfelex@gmail.com>
2025-03-26 11:11:32 +0300

    working fine

--- Day 36 of Js/app.js
+++ Day 36 of Js/app.js
@@ -1,11 +1,10 @@
-let tweet = document.getElementById("tweet");
-let p = document.querySelectorAll("p");
-let tweetlenght = 60;
-let span = document.querySelector("span");
+let tweet = document.getElementById("product-name");
+let remainingChar = document.querySelector(".count");
+
 function inputBox(e) {
     console.log(e.target.value.length);
-    p[0].innerHTML = tweetlenght - e.target.value.length;
-    span.innerHTML = e.target.value.length;
+    remainingChar.innerHTML = 60 - e.target.value.length;
+    remainingChar.innerHTML = e.target.value.length;
 }
 tweet.addEventListener("input", inputBox
 
