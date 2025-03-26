9729af605474e94981b71d254c0237744d39e077
Felex Kuria <engineerfelex@gmail.com>
2025-03-26 10:47:30 +0300

    working fine  just styling

--- Day 36 of Js/app.js
+++ Day 36 of Js/app.js
@@ -1,2 +1,12 @@
 let tweet = document.getElementById("tweet");
-console.log(tweet);
+let p = document.querySelectorAll("p");
+let tweetlenght = 60;
+let span = document.querySelector("span");
+function inputBox(e) {
+    console.log(e.target.value.length);
+    p[0].innerHTML = tweetlenght - e.target.value.length;
+    span.innerHTML = e.target.value.length;
+}
+tweet.addEventListener("input", inputBox
+
+);
