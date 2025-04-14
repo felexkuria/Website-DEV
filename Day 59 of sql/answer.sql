d0326a9dc513fe9d5856eac951dec18bbfb83478
Felex Kuria <engineerfelex@gmail.com>
2025-04-14 19:46:16 +0300

    day 58

---
+++ Day 59 of sql/answer.sql
@@ -0,0 +1,10 @@
+insert into reviews (reviewer_name,rating,text,date,resturant_id) values(‘felex Kuria’,1,2024-10-12,1);
+-- select * From 1st_table_name inner join  2nd_table_name on  (1st_table_name =address.id)
+
+SELECT review.reviewer_name, review.rating, review.text, review.date,
+restaurant.name, restaurant.addresses_id, restaurant.type_id,
+type.name
+FROM reviews AS review
+INNER JOIN restaurant AS restaurant ON review.restaurant_id = restaurant.unique_id 
+INNER JOIN types AS type ON restaurant.type_id = type.id
+WHERE rating > 3;
