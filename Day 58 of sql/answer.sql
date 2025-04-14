c7a526a8ffe458d75b774b4d7a28190e7f45bf1d
Felex Kuria <engineerfelex@gmail.com>
2025-04-14 19:20:20 +0300

    revisting data  day 58 needed ⚠️

--- Day 58 of sql/answer.sql
+++ Day 58 of sql/answer.sql
@@ -6,3 +6,13 @@
       postalCode varchar,
       country  varchar
       );
+
+     /* Inserting Data Into A Table
+* [ ✅] S23L410 - Reading Data From A Table (incl. Filtering)
+* [✅ ] S23L411 - Updating & Deleting Data
+* [ ✅] S23L412 - Designing A More Complex Database
+* [✅ ] S23L413 - Adding A New Table*/
+ insert into Addresses (street, streetNumber, city, postalCode, country)
+ values ('Main St', 123, 'Springfield', '12345', 'USA'),
+        ('Second St', 456, 'Shelbyville', '67890', 'USA'),
+     ('Third St', 789, 'Capital City', '54321', 'USA');
